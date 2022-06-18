150f90735a34cb073b14e335fe4d464df025b9fe
ArmZaDev <arztv001@gmail.com>
2022-06-18 21:29:02 +0700

    Create script characterMovement & declaration of a variable

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+
+    Animator anim;
+    CharacterController characterContro;
+
+    public float speed = 0f;
+    public float rotationSpeed = 25;
+    public float jumpSpeed = 7.5f;
+    public float garvity = 20.0f;
+
+    Vector3 inputVector;
+    Vector3 targetDirection;
+
+    private Vector3 moveDirection = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
