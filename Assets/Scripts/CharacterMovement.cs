385956a1a86f248330c550e3db872863645d23c6
ArmZaDev <arztv001@gmail.com>
2022-06-18 21:58:35 +0700

    Charactermovement Moving, Running

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,9 +6,9 @@ public class CharacterMovement : MonoBehaviour
 {
 
     Animator anim;
-    CharacterController characterContro;
+    CharacterController characterController;
 
-    public float speed = 0f;
+    public float speed = 6.0f;
     public float rotationSpeed = 25;
     public float jumpSpeed = 7.5f;
     public float garvity = 20.0f;
@@ -21,12 +21,35 @@ public class CharacterMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1;
+        anim = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float x = -(Input.GetAxisRaw("Vertical"));
+        float z = Input.GetAxisRaw("Horizontal");
+        inputVector = new Vector3(x, 0, z);
+
+        anim.SetFloat("Input X", z);
+        anim.SetFloat("Input Z", -(x));
         
+        if (x != 0 || z != 0)
+        {
+            anim.SetBool("Moving", true);
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Moving", false);
+            anim.SetBool("Running", false);
+        }
+
+        
+
     }
+
+    
 }
